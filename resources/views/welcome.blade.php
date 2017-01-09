fa9a32f2500a50f3a61433a91a214b1a3944b40a
supuy-ruby <87351750@qq.com>
2017-01-09 15:47:47 +0800

    'aaa'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">My World,I Come Back!wangwang</div>
+                <div class="title">My World,I Come Back!wawa</div>
             </div>
         </div>
     </body>
