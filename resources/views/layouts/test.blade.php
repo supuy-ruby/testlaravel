b652f5083a3c85384685e3a1eda7d1eae0e1e8fe
刘新 <liuxin@shoucq.com>
2017-01-09 14:50:50 +0800

    tianjiashitu

---
+++ resources/views/layouts/test.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>@yield('title','测试好了')</title>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
---
+++ resources/views/static_pages/default.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.test')
+@section('title')我是主页@stop
+@section('content')
+<h1>测试成功</h1>
+@stop
